ffc793eeb1aeee6635b4af3721ebc9a9191b350d
Maksym Trofimenko <maksym@container-registry.com>
2024-09-07 11:16:41 +0100

    update oapi-codegen

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	_ "github.com/deepmap/oapi-codegen/pkg/codegen" // to avoid go mod tidy clean it up
+	_ "github.com/oapi-codegen/oapi-codegen/v2/pkg/codegen" // to avoid go mod tidy clean it up
 )
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=codegen-config.yaml api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=codegen-config.yaml api.yaml
